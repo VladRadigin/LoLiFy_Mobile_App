b4d6ced3bb5ced84f0744054cbb11c2e0749a3e3
VladRadigin <angry.gamer.comi@gmail.com>
2022-02-19 18:36:21 +0600

    Automatic detection of mobile screen height and width

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../data.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -8,10 +9,25 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  var _deviceHeight;
+  var _deviceWidth;
+
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Container(),
+      body: Stack(
+        children: <Widget>[],
+      ),
+    );
+  }
+
+  Widget _featuredLoLWidget() {
+    return SizedBox(
+      height: _deviceHeight,
+      width: _deviceWidth,
+      child: Container(),
     );
   }
 }
